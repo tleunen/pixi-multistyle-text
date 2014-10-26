aa58e6be2b317e9504019bd6e755f4b92d830d91
tleunen <tommy.leunen+github@gmail.com>
2014-10-26 18:19:19 -0400

    initial commit with v0.1.0

---
+++ README.md
@@ -0,0 +1,60 @@
+# pixi-multistyle-text
+
+[![NPM](https://nodei.co/npm/pixi-multistyle-text.png)](https://nodei.co/npm/pixi-multistyle-text/)
+
+Add a `MultiStyleText` object inside [pixi.js](https://github.com/GoodBoyDigital/pixi.js) to easily create text using different styles.
+
+## Example
+
+In the example below, we are defining 4 text styles.
+`def` is the default style for the text, and the others matches the tags inside the text.
+
+```js
+var textSample = new PIXI.MultiStyleText("<pixi>Pixi.js</pixi> can has <multiline>multiline</multiline>\nand <multistyle>multi-styles</multistyle> text!",
+    {
+        def: { font: "35px Snippet", fill: "white" },
+        multiline: { font: "35px Snippet", fill: "blue" },
+        pixi: { font: "35px Snippet", fill: "#D90057" },
+        multistyle: { font: "35px Snippet", fill: "red" }
+    }
+);
+```
+
+## Usage
+
+### `text = new PIXI.MultiStyleText(text, [globalStyle,] textStyles)`
+
+Creates a new Multi-Styles Text with the given options
+
+#### `globalStyle`
+Type: `[Object]`
+
+- `align` {string} Alignment for multiline text ('left', 'center' or 'right'), does not affect single line text. Default left.
+- `wordWrap` {boolean} Indicates if word wrap should be used. Default false.
+- `wordWrapWidth` {Number} The width at which text will wrap. Default 100.
+
+#### `textStyles`
+Type: `[String:Object]`
+
+Each key of this dictionary should match with a tag in the text. Use the key `def` for the default style.
+
+The style object can have these properties:
+
+- `font` {string} The style and size of the font. Default bold 20pt Arial.
+- `fill` {object} A canvas fillstyle that will be used on the text. Can be a color, a gradient, or a pattern. Default black.
+- `stroke` {object} A canvas fillstyle that will be used on the text stroke. Can be a color, a gradient, or a pattern. Default black.
+- `strokeThickness` {number} A number that represents the thickness of the stroke. Default 0.
+- `dropShadow` {boolean} Set a drop shadow for the text. Default false.
+- `dropShadowColor` {object} A fill style to be used on the dropshadow. Can be a color, a gradient, or a pattern. Default black.
+- `dropShadowAngle` {number} Set a angle of the drop shadow. Default Math.PI/4.
+- `dropShadowDistance` {number} Set a distance of the drop shadow. Default 5.
+
+## demo
+
+- Open `demo/index.html` in a browser and you should see something like this:
+
+![img](http://i.imgur.com/lQZeHZ1.jpg)
+
+## License
+
+MIT, see [LICENSE.md](http://github.com/tleunen/pixi-multistyle-text/blob/master/LICENSE.md) for details.
---
+++ pixi-multistyle-text.js
@@ -0,0 +1,306 @@
+/**
+ * @license
+ * pixi-multistyle-text - v0.1.0
+ * Copyright (c) 2014, Tommy Leunen <tommy.leunen@gmail.com> (http://tommyleunen.com/)
+ * Released under the MIT license
+ * See https://github.com/tleunen/pixi-multistyle-text for more details
+ */
+(function(){
+    var root = this;
+    var PIXI = root.PIXI;
+
+if(!PIXI) return;
+
+/**
+ * A Multi-Style Text Object will create a line or multiple lines of text, using tags to specify different styles.
+ * A tag is similar to an html tag, except you can use whatever keyword you want. (e.g. <myTag>My text</myTag>)
+ *
+ * @class MultiStyleText
+ * @extends Text
+ * @constructor
+ * @param text {String} The copy that you would like the text to display
+ * @param [globalStyle] {Object} The global style parameters
+ * @param [globalStyle.align='left'] {String} Alignment for multiline text ('left', 'center' or 'right'), does not affect single line text
+ * @param [globalStyle.wordWrap=false] {Boolean} Indicates if word wrap should be used
+ * @param [globalStyle.wordWrapWidth=100] {Number} The width at which text will wrap, it needs wordWrap to be set to true
+ * @param [textStyles] {Object} The text styles object parameters. A key of this object is a tag name, and the content must be a style object. The key `def` specifies the default styles. The style object is the same as the one in Pixi.Text.
+ */
+var MultiStyleText = function(text, globalStyle, textStyles)
+{
+    PIXI.Text.call(this, text, !textStyles ? null : globalStyle);
+
+    this.setTextStyles(textStyles || globalStyle);
+};
+
+// constructor
+MultiStyleText.prototype = Object.create(PIXI.Text.prototype);
+MultiStyleText.prototype.constructor = MultiStyleText;
+
+/**
+ * Set the global style of the text
+ *
+ * @method setStyle
+ * @param [style] {Object} The global style parameters
+ * @param [style.align='left'] {String} Alignment for multiline text ('left', 'center' or 'right'), does not affect single line text
+ * @param [style.wordWrap=false] {Boolean} Indicates if word wrap should be used
+ * @param [style.wordWrapWidth=100] {Number} The width at which text will wrap
+ */
+MultiStyleText.prototype.setStyle = function(style)
+{
+    style = style || {};
+    style.align = style.align || 'left';
+    style.wordWrap = style.wordWrap || false;
+    style.wordWrapWidth = style.wordWrapWidth || 100;
+
+    this.style = style;
+    this.dirty = true;
+};
+
+function setDefaultTextStyle(style) {
+    style = style || {};
+    style.font = style.font || 'bold 20pt Arial';
+    style.fill = style.fill || 'black';
+    style.stroke = style.stroke || 'black'; //provide a default, see: https://github.com/GoodBoyDigital/pixi.js/issues/136
+    style.strokeThickness = style.strokeThickness || 0;
+
+    style.dropShadow = style.dropShadow || false;
+    style.dropShadowAngle = style.dropShadowAngle || Math.PI / 6;
+    style.dropShadowDistance = style.dropShadowDistance || 4;
+    style.dropShadowColor = style.dropShadowColor || 'black';
+}
+
+/**
+ * Set the text styles for each tag
+ * Use the key `def` to specify the default styles
+ *
+ * @method setTextStyles
+ * @param [styles] {Object.<string,Style>} The style map where the key is the tag name.
+ */
+MultiStyleText.prototype.setTextStyles = function(styles)
+{
+    for(var styleId in styles) {
+        if(styles.hasOwnProperty(styleId)) {
+            setDefaultTextStyle(styles[styleId]);
+        }
+    }
+
+    // we need a `def` style
+    if(!styles.def) {
+        styles.def = {};
+        setDefaultTextStyle(styles.def);
+    }
+
+    this.textStyles = styles;
+    this.dirty = true;
+};
+
+/**
+ * Get the text data with each text group using specific styles
+ *
+ * @private
+ * @method _getTextDataPerLine
+ * @param [lines] {Array} The lines of text
+ */
+MultiStyleText.prototype._getTextDataPerLine = function(lines) {
+    var outputTextData = [];
+
+    var re = /<\/?([a-z]+)>/g;
+    var currentStyle = this.textStyles.def;
+
+    // determine the group of word for each line
+    for(var i=0; i<lines.length; i++) {
+        var lineTextData = [];
+
+        // find tags inside the string
+        var matches = [];
+        var matchArray;
+        while ( (matchArray = re.exec(lines[i])) !== null && matches.push(matchArray));
+
+        // if there is no match, we still need to add the line with the default style
+        if(!matches.length) {
+            lineTextData.push({
+                text: lines[i],
+                style: currentStyle
+            });
+        }
+        else {
+            // We got a match! add the text with the needed style
+            var currentSearchIdx = 0;
+            for(var j=0; j<matches.length; j++) {
+                if(matches[j].index > currentSearchIdx) {
+                    lineTextData.push({
+                        text: lines[i].substring(currentSearchIdx, matches[j].index),
+                        style: currentStyle
+                    });
+                }
+
+                // reset the style if end of tag
+                if(matches[j][0][1] == '/') currentStyle = this.textStyles.def;
+                // set the current style
+                else currentStyle = this.textStyles[matches[j][1]] || this.textStyles.def;
+
+                // update the current search index
+                currentSearchIdx = matches[j].index + matches[j][0].length;
+            }
+
+            // is there any character left?
+            if(currentSearchIdx < lines[i].length) {
+                lineTextData.push({
+                    text: lines[i].substring(currentSearchIdx),
+                    style: this.textStyles.def
+                });
+            }
+        }
+
+        outputTextData.push(lineTextData);
+    }
+
+    return outputTextData;
+};
+
+
+/**
+ * Renders text and updates it when needed
+ *
+ * @method updateText
+ * @private
+ */
+MultiStyleText.prototype.updateText = function()
+{
+    this.texture.baseTexture.resolution = this.resolution;
+    var outputText = this.text;
+    var textStyles = this.textStyles;
+    var i, j;
+
+    // word wrap
+    // preserve original text
+    if(this.style.wordWrap) outputText = this.wordWrap(this.text);
+
+    //split text into lines
+    var lines = outputText.split(/(?:\r\n|\r|\n)/);
+
+    // get the text data with specific styles
+    var outputTextData = this._getTextDataPerLine(lines);
+
+    // calculate text width and height
+    var lineWidths = [];
+    var lineHeights = [];
+    var maxLineWidth = 0;
+    for(i=0; i<lines.length; i++) {
+        var lineWidth = 0;
+        var lineHeight = 0;
+        for(j=0; j<outputTextData[i].length; j++) {
+            this.context.font = outputTextData[i][j].style.font;
+
+            // add lineWidth inside the text data
+            outputTextData[i][j].width = this.context.measureText(outputTextData[i][j].text).width;
+            lineWidth += outputTextData[i][j].width;
+
+            // add the fontProperties inside the text data
+            outputTextData[i][j].fontProperties = this.determineFontProperties(outputTextData[i][j].style.font);
+            lineHeight = Math.max(lineHeight, outputTextData[i][j].fontProperties.fontSize + outputTextData[i][j].style.strokeThickness);
+        }
+
+        lineWidths[i] = lineWidth;
+        lineHeights[i] = lineHeight;
+        maxLineWidth = Math.max(maxLineWidth, lineWidth);
+    }
+
+    // transform styles in array
+    var stylesArray = Object.keys(textStyles).map(function(k) {
+        return textStyles[k];
+    });
+
+
+    var maxStrokeThickness = stylesArray.reduce(function(prev, curr) {
+        return Math.max(prev, curr.strokeThickness);
+    }, 0);
+    var maxDropShadowDistance = stylesArray.reduce(function(prev, curr) {
+        var value = curr.dropShadow ? curr.dropShadowDistance : 0;
+        return Math.max(prev, value);
+    }, 0);
+
+    // define the right width and height
+    var width = maxLineWidth + maxStrokeThickness + maxDropShadowDistance;
+    var height = (Math.max.apply(null, lineHeights) * lines.length) + maxDropShadowDistance;
+
+    this.canvas.width = ( width + this.context.lineWidth ) * this.resolution;
+    this.canvas.height = height * this.resolution;
+
+    this.context.scale( this.resolution, this.resolution);
+
+    if(navigator.isCocoonJS) this.context.clearRect(0,0,this.canvas.width,this.canvas.height);
+
+
+    this.context.textBaseline = 'alphabetic';
+    this.context.lineJoin = 'round';
+
+    // Draw the text
+    var linePositionX;
+    var linePositionY;
+    for(i=0; i<outputTextData.length; i++) {
+        var line = outputTextData[i];
+        linePositionX = 0;
+
+        for(j=0; j<line.length; j++) {
+            var textStyle = line[j].style;
+            var text = line[j].text;
+            var fontProperties = line[j].fontProperties;
+
+            this.context.font = textStyle.font;
+            this.context.strokeStyle = textStyle.stroke;
+            this.context.lineWidth = textStyle.strokeThickness;
+
+            linePositionX += maxStrokeThickness / 2;
+            linePositionY = (maxStrokeThickness / 2 + i * lineHeights[i]) + fontProperties.ascent;
+
+            if(this.style.align === 'right')
+            {
+                linePositionX += maxLineWidth - lineWidths[i];
+            }
+            else if(this.style.align === 'center')
+            {
+                linePositionX += (maxLineWidth - lineWidths[i]) / 2;
+            }
+
+            // draw shadow
+            if(textStyle.dropShadow) {
+                this.context.fillStyle = textStyle.dropShadowColor;
+
+                var xShadowOffset = Math.sin(textStyle.dropShadowAngle) * textStyle.dropShadowDistance;
+                var yShadowOffset = Math.cos(textStyle.dropShadowAngle) * textStyle.dropShadowDistance;
+
+                if(textStyle.fill)
+                {
+                    this.context.fillText(text, linePositionX + xShadowOffset, linePositionY + yShadowOffset);
+                }
+            }
+
+            // set canvas text styles
+            this.context.fillStyle = textStyle.fill;
+
+            // draw lines
+            if(textStyle.stroke && textStyle.strokeThickness)
+            {
+                this.context.strokeText(text, linePositionX, linePositionY);
+            }
+
+            if(textStyle.fill)
+            {
+                this.context.fillText(text, linePositionX, linePositionY);
+            }
+
+            // set Position X to the line width
+            // remove the strokeThickness otherwise the text will be to far from the previous group
+            linePositionX += line[j].width;
+            linePositionX -= maxStrokeThickness / 2;
+        }
+    }
+
+    this.updateTexture();
+};
+
+
+PIXI.MultiStyleText = MultiStyleText;
+
+}).call(this);
