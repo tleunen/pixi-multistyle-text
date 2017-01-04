cd7d1599e974a900098429707bb233038b80b672
Tommy Leunen <tommy.leunen+github@gmail.com>
2017-01-03 23:26:50 -0500

    Add initial circleci config with happo

---
+++ scripts/happo-ci.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Exit as soon as the script fails
+set -ex
+
+if [ -z "$CI_PULL_REQUEST" ]; then
+  echo "This is not a pull request build."
+  exit 0
+fi
+
+happo_run() {
+  echo "Checking out $1"
+
+  git checkout --quiet "$1"
+
+  npm install
+  npm run build
+
+  # Run Happo for the current commit. We use `xvfb-run` so that we can run
+  # Happo (which uses Firefox) in a headless display environment.
+  xvfb-run -a happo run
+}
+
+echo "Running Happo on latest master"
+happo_run origin/master
+
+echo "Running Happo on current PR commit ($CIRCLE_SHA1)"
+happo_run "$CIRCLE_SHA1"
