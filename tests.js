a4a92e61607099c00e904537cd9397358f2c5fb5
bluepichu <github@bluepichu.com>
2017-01-19 15:59:17 -0500

    Simplify testing structure

--- tests.js
+++ tests.js
@@ -1,12 +1,30 @@
-happo.define("Basic example", () => {
-	document.body.innerHTML = "<div id='root'></div>";
-	PIXI.settings.RESOLUTION = 2;
-	let renderer = PIXI.autoDetectRenderer(800, 400);
-	renderer.backgroundColor = 0x333333;
-	document.getElementById("root").appendChild(renderer.view);
-	let stage = new PIXI.Container();
+const WIDTH = 600;
+const HEIGHT = 500;
 
-	let text = new MultiStyleText("Let's make some <ml>multiline</ml>\nand <ms>multistyle</ms> text for\n<pixi>Pixi.js!</pixi>",
+function test(fn) {
+	return () => {
+		document.body.innerHTML = "<div id='root'></div>";
+		PIXI.settings.RESOLUTION = 2;
+		let renderer = PIXI.autoDetectRenderer(2 * WIDTH, 2 * HEIGHT);
+		renderer.backgroundColor = 0x333333;
+		document.getElementById("root").appendChild(renderer.view);
+
+		let stage = new PIXI.Container();
+		let text = fn();
+
+		text.x = (WIDTH - text.width) / 2;
+		text.y = 20;
+
+		debugger;
+
+		stage.addChild(text);
+
+		renderer.render(stage);
+	}
+}
+
+happo.define("Basic example", test(() =>
+	new MultiStyleText("Let's make some <ml>multiline</ml>\nand <ms>multistyle</ms> text for\n<pixi>Pixi.js!</pixi>",
 	{
 		"default": {
 			fontFamily: "Arial",
@@ -26,24 +44,11 @@ happo.define("Basic example", () => {
 			fontSize: "64px",
 			fill: "#efefef"
 		}
-	});
+	})
+));
 
-	text.x = 200 - text.width / 2;
-	text.y = 10;
-	stage.addChild(text);
-
-	renderer.render(stage);
-});
-
-happo.define("Nested tags", () => {
-	document.body.innerHTML = "<div id='root'></div>";
-	PIXI.settings.RESOLUTION = 2;
-	let renderer = PIXI.autoDetectRenderer(1200, 400);
-	renderer.backgroundColor = 0x333333;
-	document.getElementById("root").appendChild(renderer.view);
-	let stage = new PIXI.Container();
-
-	let nested = new MultiStyleText("You can <outline>nest <b>tags <red>as <i>deeply <thicker>as <shadow>you'd <large>like</large></shadow></thicker></i></red></b></outline>",
+happo.define("Nested tags", test(() =>
+	new MultiStyleText("You can <outline>nest <b>tags <red>as <i>deeply <thicker>as <shadow>you'd <large>like</large></shadow></thicker></i></red></b></outline>",
 	{
 		"default": {
 			fontFamily: "Arial",
@@ -58,24 +63,11 @@ happo.define("Nested tags", () => {
 		"thicker": { strokeThickness: 6 },
 		"shadow": { dropShadow: true, dropShadowColor: "#888888" },
 		"large": { fontSize: "36px" }
-	});
+	})
+));
 
-	nested.x = 300 - nested.width / 2;
-	nested.y = 20;
-	stage.addChild(nested);
-
-	renderer.render(stage);
-});
-
-happo.define("Vertical alignment", () => {
-	document.body.innerHTML = "<div id='root'></div>";
-	PIXI.settings.RESOLUTION = 2;
-	let renderer = PIXI.autoDetectRenderer(1200, 400);
-	renderer.backgroundColor = 0x333333;
-	document.getElementById("root").appendChild(renderer.view);
-	let stage = new PIXI.Container();
-
-	let valign = new MultiStyleText("You can use <code>valign</code> to control the <top>vertical</top> <middle>alignment</middle> of <bottom>text</bottom>.",
+happo.define("Vertical alignment", test(() =>
+	new MultiStyleText("You can use <code>valign</code> to control the <top>vertical</top> <middle>alignment</middle> of <bottom>text</bottom>.",
 	{
 		"default": {
 			fontFamily: "Arial",
@@ -86,24 +78,11 @@ happo.define("Vertical alignment", () => {
 		"top": { fontSize: "14px", valign: "top" },
 		"middle": { fontSize: "14px", valign: "middle" },
 		"bottom": { fontSize: "14px", valign: "bottom" }
-	});
+	})
+));
 
-	valign.x = 300 - valign.width / 2;
-	valign.y = 20;
-	stage.addChild(valign);
-
-	renderer.render(stage);
-});
-
-happo.define("Wrapping and alignment", () => {
-	document.body.innerHTML = "<div id='root'></div>";
-	PIXI.settings.RESOLUTION = 2;
-	let renderer = PIXI.autoDetectRenderer(800, 600);
-	renderer.backgroundColor = 0x333333;
-	document.getElementById("root").appendChild(renderer.view);
-	let stage = new PIXI.Container();
-
-	let wrapping = new MultiStyleText("Global word wrap and alignment properties are controlled by the \"default\" style, and can't be overridden by other styles.",
+happo.define("Wrapping and alignment", test(() =>
+	new MultiStyleText("Global word wrap and alignment properties are controlled by the \"default\" style, and can't be overridden by other styles.",
 	{
 		"default": {
 			fontFamily: "Arial",
@@ -113,24 +92,12 @@ happo.define("Wrapping and alignment", () => {
 			wordWrapWidth: 150,
 			align: "right"
 		}
-	});
+	})
+));
 
-	wrapping.x = 350 - wrapping.width;
-	wrapping.y = 20;
-	stage.addChild(wrapping);
-
-	renderer.render(stage);
-});
-
-happo.define("Have fun", () => {
-	document.body.innerHTML = "<div id='root'></div>";
-	PIXI.settings.RESOLUTION = 2;
-	let renderer = PIXI.autoDetectRenderer(1200, 400);
-	renderer.backgroundColor = 0x333333;
-	document.getElementById("root").appendChild(renderer.view);
-	let stage = new PIXI.Container();
-
-	let funStyles = {
+happo.define("Have fun", test(() =>
+	new MultiStyleText("Now have fun making some <blue>beautiful</blue> <red>multistyle</red> text!",
+	{
 		"default": {
 			fontFamily: "Arial",
 			fontSize: "24px",
@@ -144,13 +111,5 @@ happo.define("Have fun", () => {
 		},
 		"blue": { fill: 0x4488ff, stroke: 0x2244cc },
 		"red": { fill: 0xff8888, stroke: 0xcc4444 }
-	};
-
-	let fun = new MultiStyleText("Now have fun making some <blue>beautiful</blue> <red>multistyle</red> text!", funStyles);
-
-	fun.x = 300 - fun.width / 2;
-	fun.y = 20;
-	stage.addChild(fun);
-
-	renderer.render(stage);
-});
+	})
+));
