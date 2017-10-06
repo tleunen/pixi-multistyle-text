2f5ec3740d3da8236d455eb2477873130b9eed9c
Matthew Savage <github@bluepichu.com>
2017-10-06 14:19:24 -0400

    Feat: Add testing and CI
    May or may not work; merging so that we have something to compare against in diffs.

---
+++ .happo.js
@@ -0,0 +1,19 @@
+module.exports = {
+	bind: "localhost",
+	driver: "chrome",
+	port: 4567,
+	sourceFiles: [
+		"node_modules/pixi.js/dist/pixi.js",
+		"dist/pixi-multistyle-text.js",
+		"tests.js"
+	],
+	stylesheets: [],
+	publicDirectories: [],
+	snapshotsFolder: 'happo-snapshots',
+	viewports: {
+		large: {
+			width: 2048,
+			height: 2048,
+		}
+	},
+};
